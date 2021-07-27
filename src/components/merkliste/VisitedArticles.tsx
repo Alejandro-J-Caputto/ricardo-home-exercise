95fb6e38ba7d4c1ac6d641f976913eba3dfc33e9
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-07-27 20:06:01 +0200

    typos and merkliste

--- src/components/merkliste/VisitedArticles.tsx
+++ src/components/merkliste/VisitedArticles.tsx
@@ -3,7 +3,7 @@ import { setArticleIdAsync } from "../../redux/actions/articlesActions";
 import { RootState } from "../../redux/store/store";
 import { SearchArticle } from "../../types/response.types";
 import SavedItems from "./SavedItems";
-
+import placeholderIMG from "../../assets/img/Data Collecting_Two Color.svg";
 const VisitedArticles = () => {
   const articlesState = useSelector((state: RootState) => state.articles);
   const { selectedItemsLocalStorage } = articlesState;
@@ -14,20 +14,28 @@ const VisitedArticles = () => {
     dbDispatchLocalStorage(setArticleIdAsync(id, article));
   };
   //TODO:USE EVENT DELEGATION, ONE SINGLE EVENT LISTENER, PERFORMANCE COST
-  const UI =  useSelector((state:RootState) => state.uiLoading);
+  const UI = useSelector((state: RootState) => state.uiLoading);
 
-  const {uiTheme} = UI;
+  const { uiTheme } = UI;
   return (
-    <ul className="marked-items__list">
-      {selectedItemsLocalStorage.map((el: SearchArticle) => (
-        <SavedItems
-          key={el.id}
-          itemsContent={el}
-          uiTheme={uiTheme}
-          onSelect={selectItemHandler}
-        />
-      ))}
-    </ul>
+    <div>
+      {!selectedItemsLocalStorage.length ? (
+        <div className="articles-empty">
+          <h1>Please, add an item first</h1>
+          <img src={placeholderIMG} alt="ilustration" />
+        </div>
+      ) : null}
+      <ul className="marked-items__list">
+        {selectedItemsLocalStorage.map((el: SearchArticle) => (
+          <SavedItems
+            key={el.id}
+            itemsContent={el}
+            uiTheme={uiTheme}
+            onSelect={selectItemHandler}
+          />
+        ))}
+      </ul>
+    </div>
   );
 };
 
