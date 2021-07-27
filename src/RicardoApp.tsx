d745e06c54e899b26d7c836b6adaf265a0ef823e
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-07-27 19:12:31 +0200

    trying to deploy

--- src/RicardoApp.tsx
+++ src/RicardoApp.tsx
@@ -24,7 +24,7 @@ const RicardoApp: React.FC = () => {
               )}
             />
             <Route
-              path="/"
+              path="/home-exercise"
               component={(props: RouteComponentProps) => (
                 <HomeRouter routing={props} />
               )}
